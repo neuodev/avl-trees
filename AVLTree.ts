3ff11a16d1a5fa42c05241c184346e619f75b5f1
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-08 19:07:10 +0200

    Apply insert method using recursion

--- AVLTree.ts
+++ AVLTree.ts
@@ -1,11 +1,38 @@
 class Leaf {
   val: number;
-  right: any;
-  left: any;
+  right: Leaf;
+  left: Leaf;
   constructor(val) {
     this.val = val;
     this.right = null;
     this.left = null;
   }
 }
-class AVLTree {}
+class AVLTree {
+  root: Leaf;
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    this.root = this._insert(this.root, val);
+  }
+
+  _insert(node, val) {
+    if (!node) return new Leaf(val);
+    if (val < node.val) {
+      node.left = this._insert(node.left, val);
+    } else {
+      node.right = this._insert(node.right, val);
+    }
+
+    return node;
+  }
+}
+
+let avlTree = new AVLTree();
+avlTree.insert(1);
+avlTree.insert(2);
+avlTree.insert(-1);
+
+
